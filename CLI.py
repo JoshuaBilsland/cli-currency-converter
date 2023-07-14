ba9201f7ec9408ba93041e7e93fae19df5a5f414
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-07-14 09:22:32 +0100

    Create venv, CLI class and app banner

---
+++ CLI.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+class CommandLineInterface:
+    def __init__(self):
+        previousConversions = []
+        self.__displayProgramBanner()
+        
+        
+    def __displayProgramBanner(self):    
+        text = "Currency Converter"
+        padding = len(text)//4
+        bannerWidth = int(len(text)*1.5)
+        
+        # Print the top border
+        print("=" * bannerWidth)
+        
+        # Print the text
+        print("|" + " " * padding + text + " " * (padding-1) + "|")
+        
+        # Print the bottom border
+        print("=" * bannerWidth)
+        
+        
+if __name__ == "__main__":
+    commandLineInterface = CommandLineInterface()
+    print(sys.path)
